3a829cdbd7bc12079a8257c6402639741b30f586
Adoboxd <axelxand@gmail.com>
2025-05-09 16:49:28 +0800

    Add files via upload

---
+++ reservation/src/reservation/FXMLDocumentController.java
@@ -0,0 +1,20 @@
+package reservation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class FXMLDocumentController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
